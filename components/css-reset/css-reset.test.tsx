043b3d6fce9b52842845b12cd3d1356a63400841
Luke <lukepbayliss@gmail.com>
2019-11-15 07:01:49 +1100

    Adds husky back, adds tests.

--- components/css-reset/css-reset.test.tsx
+++ components/css-reset/css-reset.test.tsx
@@ -1,1 +1,11 @@
-export default null;
+import { shallow } from 'enzyme';
+
+import { default as CSSReset } from './css-reset.component';
+
+describe('Button Component', () => {
+  it('Renders button with correct label text', () => {
+    const component = shallow(<CSSReset />);
+
+    expect(component.exists()).toBeTruthy();
+  });
+});
--- components/page-layout/page-layout.test.tsx
+++ components/page-layout/page-layout.test.tsx
@@ -1,1 +1,11 @@
-export default null;
+import { shallow } from 'enzyme';
+
+import { default as PageLayout } from './page-layout.component';
+
+describe('Button Component', () => {
+  it('Renders button with correct label text', () => {
+    const component = shallow(<PageLayout />);
+
+    expect(component.exists()).toBeTruthy();
+  });
+});
